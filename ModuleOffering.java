fa3cc214287123d349acc9420ca00626437ed64a
TronaruDavid <140056478+TronaruDavid@users.noreply.github.com>
2025-11-17 13:58:51 +0000

    Uncommented and added the java docs

--- ModuleOffering.java
+++ ModuleOffering.java
@@ -1,23 +1,32 @@
 import java.util.ArrayList;
 
-/* public class ModuleOffering {
+/**
+ * A class that represents the actual module entity in the timetable
+ * */
+public class ModuleOffering {
     Module module;
-    Semester semester;
+    private int semesterNumber;
     ArrayList<Session> sessions;
 
-    public ModuleOffering(Module module, Semester semester, ArrayList<Session> sessions) {
+    /**
+     * Creates a ModuleOffering object with the following arguments
+     * @param module the module it represents
+     * @param semesterNumber the semester it belongs to
+     * @param sessions the list of labs/tutorials/labs this module has
+     * */
+    public ModuleOffering(Module module, int semesterNumber, ArrayList<Session> sessions) {
         this.module = module;
-        this.semester = semester;
+        this.semesterNumber = semesterNumber;
         this.sessions = sessions;
     }
 
-    public void addSession(Session session) {
+    /* public void addSession(Session session) {
         sessions.add(session);
     }
 
     public ArrayList<Session> getSessions() {
         return sessions;
-    }
-} */
+    } */
+}
 
 
