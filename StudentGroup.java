4b7824a8955280bc2bef816f3fac445ba74dc047
Mickl5 <micklawlor31@gmail.com>
2025-11-17 13:20:21 +0000

    Add files via upload

--- StudentGroup.java
+++ StudentGroup.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * represents a group of students for a specific subject and year.
+ * It also stores the sessions that this group attends.
+ */
+
 public class StudentGroup {
 
     private String groupId;
@@ -8,13 +13,22 @@ public class StudentGroup {
     private SubjectsYear subjectYear;
     private int noOfStudents;
     private List<Session> sessions;
+    private List<StudentGroup> subGroups
 
+    /**
+     * creates a student group
+     * @param groupId
+     * @param subject
+     * @param subjectYear
+     * @param noOfStudents
+     */
     public StudentGroup(String groupId, Subjects subject, SubjectsYear subjectYear, int noOfStudents) {
         this.groupId = groupId;
         this.subject = subject;
         this.subjectYear = subjectYear;
         this.noOfStudents = noOfStudents;
         this.sessions = new ArrayList<>();
+        this.subGroups = new ArrayList<>();
     }
 
     public String getGroupId() {
@@ -39,6 +53,14 @@ public class StudentGroup {
 
     public void addSession(Session session) {
         sessions.add(session);
+    }
+
+    public void addSubGroup(StudentGroup subGroup) {
+        subGroups.add(subGroup);
+    }
+
+    public List<StudentGroup> getSubGroups() {
+        return subGroups;
     }
 }
 
