57c6f07e9b95e1455b439a4811a27179b26233b6
TronaruDavid <140056478+TronaruDavid@users.noreply.github.com>
2025-11-17 14:00:30 +0000

    Added java docs to the Module and CSVUtils classes

--- CSVutils.java
+++ CSVutils.java
@@ -1,8 +1,14 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class CSVutils {
 
+/**
+ * An auxiliary class with only static methods used to perform operations on CSV files such as read or write
+ * */
+public class CSVutils {
+    /**
+     * @param filePath the file path to the file you want to read from
+     * */
     public static ArrayList<String[]> readCSV(String filePath) throws IOException{
         ArrayList<String[]> rows = new ArrayList<>();
 
@@ -20,6 +26,10 @@ public class CSVutils {
         return rows;
     }
 
+    /**
+     * @param filePath the file path to the file you want to read from
+     * @param data the data that will be written inside the csv
+     * */
     public static void writeCSV(String filePath, ArrayList<String[]> data) throws IOException{
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             for (String[] row : data) {
@@ -29,6 +39,10 @@ public class CSVutils {
         }
     }
 
+    /**
+     * @param filePath the file path to the file that will be appended to
+     * @param row the row that will be appended at the end of the file pointed to by the file path
+     * */
     public static void appendRow(String filePath, String[] row) throws IOException{
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
             writer.write(String.join(",", row));
@@ -36,6 +50,10 @@ public class CSVutils {
         }
     }
 
+    /**
+     * @param filePath the file path to the file that will be appended to
+     * @param rowIndex the row that will be deleted in the file pointed by the file path
+     * */
     public static void deleteRow(String filePath, int rowIndex) throws IOException {
         ArrayList<String[]> data = readCSV(filePath);
 
