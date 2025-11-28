2512eff8dcf5d54ff8b62d3d98bec587dfe29aa5
claude812 <claudejepowell@gmail.com>
2025-11-28 13:45:46 +0000

    Update subjectControl.java

--- subjectControl.java
+++ subjectControl.java
@@ -25,6 +25,7 @@ private CSVDataManager Manager;
         return null;
     }
 
+
     public boolean deleteSubject(String code) {
         Subjects subject = getSubjectByCode(code);
         if (subject != null) {
@@ -33,16 +34,45 @@ private CSVDataManager Manager;
         }
         return false;
     }
-    public void addOffering(ModuleOffering offering) {
-        Manager.getModuleOffering().add(offering);
+    public SubjectsYear getYear(int yearNumber,  String programmeId) {
+        for (SubjectsYear y : this.getSubjectByCode(programmeId).getYears()) {
+            if (y.getYearNumber() == yearNumber) {
+                return y;
+            }
+        }
+        return null;
+    }
+
+    public void addYear(int yearNumber, String programmeId) {
+       SubjectsYear year = new SubjectsYear(yearNumber);
+        this.getSubjectByCode(programmeId).getYears().add(year);
+        System.out.println("Added year " + yearNumber + " for programme " + programmeId);
+    }
+
+    public boolean removeYear(int yearNumber,  String programmeId) {
+        SubjectsYear year = getYear(yearNumber, programmeId);
+        if (year != null) {
+            this.getSubjectByCode(programmeId).getYears().remove(year);
+            System.out.println("Removed year " + yearNumber);
+            return true;
+        }
+        return false;
+    }
+
+    public void addOffering(String moduleCode,  String programmeId, int yearNumber, int semesterNumber) {
+        Module module = Manager.getModuleById(moduleCode);
+        ModuleOffering offering = new ModuleOffering(module, semesterNumber);
+        this.getSubjectByCode(programmeId).getYear(yearNumber).getModulesForSemester(semesterNumber).add(offering);
         System.out.println("Added offering: " + offering.getName());
     }
 
-    public boolean removeOffering(String offeringCode) {
-        for (ModuleOffering o : Manager.getModuleOffering()) {
-            if (o.getCode().equals(offeringCode)) {
-                Manager.getModuleOffering().remove(o);
-                System.out.println("Removed offering: " + o.getName());
+    public boolean removeOffering(String moduleCode, String programmeId, int yearNumber, int semesterNumber) {
+        Module module = Manager.getModuleById(moduleCode);
+        ModuleOffering offering = new ModuleOffering(module, semesterNumber);
+        ArrayList<ModuleOffering> offerings = this.getSubjectByCode(programmeId).getYear(yearNumber).getModulesForSemester(semesterNumber);
+        for (ModuleOffering o : offerings) {
+            if (o.getModule().getModuleCode().equals(moduleCode)) {
+                this.getSubjectByCode(programmeId).getYear(yearNumber).getModulesForSemester(semesterNumber).remove(o);
                 return true;
             }
         }
@@ -50,9 +80,9 @@ private CSVDataManager Manager;
     }
 
 }
-//Will finish tomorrow
 
 
+//Should be done now
 
 
 
