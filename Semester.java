bd1b98242a77bde6e75cc818ca034e65f2d80e19
claude812 <claudejepowell@gmail.com>
2025-11-29 15:56:23 +0000

    Update Semester.java

--- Semester.java
+++ Semester.java
@@ -1,9 +1,14 @@
+/**
+*The semester start dates and end dates are saved
+    */
 public class Semester {
     static String Sem1Start = "8th September 2025";
    static String Sem1End = "20th December 2025";
    static String Sem2Start = "26th January 2026";
     static String Sem2End = " 18th May 2026";
-
+/**
+    *this prints the semester start and end dates
+    */
     public static void main(String[] args) {
         System.out.println("Semester1 starts on the : " + Sem1Start);
         System.out.println("Semester1 ends on the : " + Sem1End);
@@ -12,3 +17,4 @@ public class Semester {
 
     }
 }
+
