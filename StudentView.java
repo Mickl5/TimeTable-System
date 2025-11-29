1e6794db77bfc4cac37a029f4ce9249cefcb1cd5
Mickl5 <micklawlor31@gmail.com>
2025-11-29 14:13:00 +0000

    Add files via upload
    finished comments

--- StudentView.java
+++ StudentView.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
 
+/**
+ *lets students view their own timetable and others
+ */
 public class StudentView extends View {
     private Scanner scanner;
     UserController userController;
     String studentGroup;
 
+    /**
+     * creats a student view
+     * @param controller used to get timetable info
+     * @param userController used to find out which user is logged in
+     */
     public StudentView(TimetableController controller, UserController userController) {
         super(controller);
         this.userController = userController;
@@ -12,15 +20,24 @@ public class StudentView extends View {
         studentGroup = userController.getCurrentUser().getLinkedId();
     }
 
+    /**
+     * shows the timetable for the logged in student
+     */
     public void viewMytimetable() {
         printTimetable(getController().getTimetableForStudent(this.studentGroup).getSessions());
     }
 
+    /**
+     * shows a menu that lets student choose what timetable to view
+     * asks for extra info if needed
+     */
     public void start(){
         System.out.println("1) View my timetable");
         System.out.println("2) View programme timetable");
         System.out.println("3) View module timetable");
         System.out.println("4) View room timetable");
+        String ans = scanner.nextLine();
+
 
         if (ans.equals("1")) {
             viewMytimetable();
